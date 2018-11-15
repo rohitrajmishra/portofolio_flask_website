a02cf56e6b8d5e072625f1f64acfe6831aadec14
rohitrajmishra <rohitrajmishra.nits@gmail.com>
2018-11-15 17:17:14 +0000

    - Added basic menu option - Added main.css

--- script1.py
+++ script1.py
@@ -1,14 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app=Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Homepage here!"
+    return render_template("home.html")
 
 @app.route('/about/')
 def about():
-    return "About Page!"
+    return render_template("about.html")
 
 if __name__=="__main__":
     app.run(debug=True)
